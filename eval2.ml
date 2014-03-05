d6e747d68c6d9d0991cc02406e46fa6788a77322
Vlad Velici <vlad.velici@gmail.com>
2014-03-05 12:38:45 +0000

    moved exceptions and typechecker out of eval2

--- eval2.ml
+++ eval2.ml
@@ -1,75 +1,8 @@
 open InterpreterObjects
 open Environment
 open Native
-
-(* Common exceptions *)
-exception CannotRedefineVariable of string;;
-exception UndefinedVariable of string;;
-exception UninitializedVariable of string;;
-exception IncompatibleTypes of tipe * tipe;; (* Expected tipe 1, found tipe 2 *)
-exception NotAFunction of string;;
-exception NotYetImplemented of expression;;
-exception NotAStream;;
-
-(* typeOf expression in some environment *)
-let rec typeOf exp env = match exp with 
-    | DeclAssign _ -> Unit 
-    | CtxDeclaration _ -> Unit 
-    | Assignment _ -> Unit
-    | VarName a -> lookup_variable_type env a
-    | ApplyFunction (name, _) -> returnType (lookup_variable_type env name) name
-    | ApplyLambda (fnc, _) -> returnType (typeOf (Primitive (ValFunction fnc)) env) "-lambda-"
-    | NewStream expr -> Stream (returnType (typeOf expr env) "-newstream-")
-    | ReadStream expr -> returnTypeStream expr env
-    | Primitive (ValInt _) -> Int
-    | Primitive (ValFloat _) -> Float
-    | Primitive (ValBoolean _) -> Boolean
-    | Primitive (Null) -> Unit
-    | Primitive (Undefined) -> Unit
-    | Primitive (ValStream (t, _)) -> Stream t
-    | Primitive (ValFunction (Func (t, arglist, _))) -> Function (t, (typelist_of_arglist arglist))
-    | _ -> raise (NotYetImplemented exp)
-
-
-and is_stream expr env = match expr with
-    | NewStream _ -> true
-    | VarName name -> (match (lookup_variable_type env name) with Stream _ -> true | _ -> false)
-    | Primitive (ValStream _) -> true
-    | _ -> false
-
-(* return type of a given function *)
-and returnType fnc name = match fnc with
-    | Function (t, _) -> t
-    | _ -> raise (NotAFunction name)
-
-and returnTypeStream expr env = 
-    let stream = typeOf expr env in (match stream with Stream t -> t | _ -> raise NotAStream)
-
-(* helpers for working with functions *)
-and value_of_var = function (_, v) -> v
-and type_of_var = function (t, _) -> t
-
-(* whether the type of Primitive is a function or not *)
-and is_function = function
-    | Function _ -> true
-    | _ -> false
-
-(* given an arugmnet list, return a list of their types only *)
-and typelist_of_arglist = function
-    | Argument (_, t) :: l -> t :: typelist_of_arglist l
-    | [] -> [];;
-
-(* make sure arguments types match with function arguments *)
-let rec validate_parameters args params env = match args, params with
-    | [], [] -> true
-    | Argument(_, t_arg) :: arglist, expr :: paramlist -> (types_compatible t_arg (typeOf expr env)) && (validate_parameters arglist paramlist env)
-    | _, _ -> false
-
-(* check if t1 is compatible with t2 (a variable of t1 can accept a t2). Undefined and Null have type Unit which is accepted by any type *)
-and types_compatible t1 t2 = t1 = t2 || t2 = Unit
-
-(* check if t1 is the same as t2. Undefined and Null (type Unit) are not accepted as valid types for t2 *)
-and types_identical t1 t2 = t1 = t2
+open Exceptions
+open Typechecker
 
 (* EVALUATION *)
 let rec eval exp env = match exp with
@@ -83,13 +16,13 @@ let rec eval exp env = match exp with
     | VarName name -> fetch_variable env name
     | ApplyLambda (f, params) -> apply_function env env f params
     | ApplyFunction (name, params) -> let (decl_scope, (varType, f)) = lookup_variable_all env name
-        in if (is_function varType) then apply_function env decl_scope (func_of_varval f) params else raise (NotAFunction name) 
-    | NewStream expr -> if is_function (typeOf expr env) then let f = func_of_varval (eval expr env) in create_new_stream f env else raise (NotAFunction "--")
-    | ReadStream expr -> if is_stream expr env then (let stream = eval expr env in read_stream stream) else raise (NotAStream)
+        in if (type_is_function varType) then apply_function env decl_scope (func_of_varval f) params else raise (NotAFunction name) 
+    | NewStream expr -> if type_is_function (typeOf expr env) then let f = func_of_varval (eval expr env) in create_new_stream f env else raise (NotAFunction "--")
+    | ReadStream expr -> if type_is_stream expr env then (let stream = eval expr env in read_stream stream) else raise (NotAStream)
     | Primitive p -> p
     | tmp -> raise (NotYetImplemented tmp)
 
-(*todo: typecheck this*)
+(* Reads the next element from a stream *)
 and read_stream stream = match stream with
     | ValStream (t, s) -> (try (Stream.next s) with Stream.Failure -> Null)
     | _ -> raise NotAStream
@@ -146,7 +79,7 @@ and eval_func_body scope body : varValue= match body with
 and apply_function call_scope decl_scope f params = match f with
     | NativeFunc (_, name, arguments) -> run_native_code name params 
     | Func (rType, arguments, body) ->
-        if (validate_parameters arguments params call_scope) then
+        if (typecheck_parameters arguments params call_scope) then
             let func_scope = new_environment decl_scope
             in apply_param_bindings func_scope call_scope params arguments;
             let res = eval_func_body func_scope body
---
+++ exceptions.ml
@@ -0,0 +1,11 @@
+open InterpreterObjects
+
+(* Common exceptions *)
+exception CannotRedefineVariable of string;;
+exception UndefinedVariable of string;;
+exception UninitializedVariable of string;;
+exception IncompatibleTypes of tipe * tipe;; (* Expected tipe 1, found tipe 2 *)
+exception NotAFunction of string;;
+exception NotYetImplemented of expression;;
+exception NotAStream;;
+
